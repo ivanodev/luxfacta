b14917fd5cd5ae1feab8713690d4b5454ae4d30a
Ivano Carvalho <ivanoca.dev@gmail.com>
2020-07-31 15:31:10 -0300

    caminho de imagens

--- desktop/src/com/luxfacta/desktop/icon/Image16.java
+++ desktop/src/com/luxfacta/desktop/icon/Image16.java
@@ -1,21 +1,15 @@
 package com.luxfacta.desktop.icon;
 
 
-import java.net.URL;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class Image16 {
 	
-	//private static String path = "C:\\tools.dev\\java\\luxfacta\\desktop\\src\\com\\luxfacta\\desktop\\icon";
-	//private static Object url = getClass().getResource("/com/luxfacta/desktop/icon");
-	
-	
-	private static Icon add16 = new ImageIcon( new URLImage().getLocation( "add16.png" ) );
-	private static Icon delete16 = new ImageIcon( new URLImage().getLocation( "delete16.png") );
-	private static Icon save16 = new ImageIcon( new URLImage().getLocation( "save16.png" ) ); 
-	private static Icon exit16 = new ImageIcon( new URLImage().getLocation( "exit16.png" ) ); 
+	private static Icon add16     = new ImageIcon( new URLImage().getLocation( "add16.png" ) );
+	private static Icon delete16  = new ImageIcon( new URLImage().getLocation( "delete16.png") );
+	private static Icon save16    = new ImageIcon( new URLImage().getLocation( "save16.png" ) ); 
+	private static Icon exit16    = new ImageIcon( new URLImage().getLocation( "exit16.png" ) ); 
 	private static Icon refresh16 = new ImageIcon( new URLImage().getLocation( "refresh16.png" ) );
 	
 	public static Icon add() {
