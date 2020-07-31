f7c871b604f9ea4af0f7715dda2a044520e9c353
Ivano Carvalho <ivano.sistemas@gmail.com>
2020-07-30 22:57:35 -0300

    aa

--- desktop/src/com/luxfacta/desktop/icon/Image16.java
+++ desktop/src/com/luxfacta/desktop/icon/Image16.java
@@ -1,26 +1,31 @@
 package com.luxfacta.desktop.icon;
 
+
+import java.net.URL;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class Image16 {
 	
-	private static String path = "C:\\tools.dev\\java\\luxfacta\\desktop\\src\\com\\luxfacta\\desktop\\icon";
+	//private static String path = "C:\\tools.dev\\java\\luxfacta\\desktop\\src\\com\\luxfacta\\desktop\\icon";
+	//private static Object url = getClass().getResource("/com/luxfacta/desktop/icon");
+	private static URL location = new URLImage().getLocation(); 
 	
-	private static Icon add16 = new ImageIcon( path + "\\add16.png");
-	private static Icon delete16 = new ImageIcon( path + "\\delete16.png");
-	private static Icon save16 = new ImageIcon( path + "\\save16.png" ); 
-	private static Icon exit16 = new ImageIcon( path + "\\exit16.png" ); 
-	private static Icon refresh16 = new ImageIcon( path + "\\refresh16.png" );
-
+	private static Icon add16 = new ImageIcon( location + "/add16.png" );
+	private static Icon delete16 = new ImageIcon( location + "/delete16.png");
+	private static Icon save16 = new ImageIcon( location + "/save16.png" ); 
+	private static Icon exit16 = new ImageIcon( location + "/exit16.png" ); 
+	private static Icon refresh16 = new ImageIcon( location + "/refresh16.png" );
+	
 	public static Icon add() {
 		
-		return add16;
+		return new ImageIcon( new URLImage().getLocation() + "/add16.png" );
 		
 	}
 	
 	public static Icon delete() {
-		
+
 		return delete16;
 		
 	}
---
+++ desktop/src/com/luxfacta/desktop/icon/URLImage.java
@@ -0,0 +1,13 @@
+package com.luxfacta.desktop.icon;
+
+import java.net.URL;
+
+public class URLImage {
+	
+	public URL getLocation() {
+		
+		return getClass().getResource( "com/luxfacta/desktop/icon" );
+		
+	}
+
+}
