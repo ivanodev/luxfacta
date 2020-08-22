006f9519ca34d40ad682162f043437b7b1f3532b
Ivano Carvalho <ivanoca.dev@gmail.com>
2020-08-22 16:44:01 -0300

    desenv

--- web/src/pages/object/EditObject.js
+++ web/src/pages/object/EditObject.js
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ObjectView } from './styles';
 import { ObjectContent } from './styles';
+import { Layout } from './styles';
 import { ObjectLayout } from './styles';
+import { Input } from './styles';
 
 function EditObject( props ) {
 
+    const [ data, setData ] = useState(null);
+
+    useEffect( () => {
+
+        const fecth = async () => {
+
+            setData({
+                _id  : 0,
+                actorType : ['company', 'customer', 'supplier', 'user'],
+                person : {
+                    fullName : 'Ivano Carvalho',
+                    federalDoc : '28731415814',
+                    stateDoc: '3013167604',
+                    cityDoc: '',
+                    phone : {
+                       residencial : '',
+                       celular: '',
+                       comercial : ''     
+                    }
+                }
+
+            });
+
+        }
+
+        fecth();
+
+    }, []);
+
     return (
-        <ObjectView id="edit-object">
-            <ObjectContent>
-               <ObjectLayout>
-                   <h1>
-                       tee
-                   </h1>
-               </ObjectLayout>
-            </ObjectContent>
-        </ObjectView>
+                <ObjectView id="edit-object">
+                    <ObjectContent>
+                        <Layout columns={2} color="red" >
+                            <ObjectLayout columns={2} color="yellow" >  
+                                { data && 
+                                    <>
+                                        <Input
+                                            value={data.person.fullName}
+                                        />
+                                        <Input
+                                            value={data.person.federalDoc}
+                                        />
+                                        <Input 
+                                            value={data.person.stateDoc}
+                                        />
+                                    </>
+                                }
+                            </ObjectLayout>
+                            <ObjectLayout columns={1} color="green" >   
+                                <Input>
+                                </Input>
+                                <Input>
+                                </Input>
+                                <Input>
+                                </Input>
+                            </ObjectLayout>
+                        </Layout>
+                        <Layout columns={1} color="red" >
+                            <ObjectLayout columns={2} color="green" >   
+                                <Input>
+                                </Input>
+                                <Input>
+                                </Input>
+                                <Input>
+                                </Input>
+                            </ObjectLayout>
+                        </Layout>
+                    </ObjectContent>
+                </ObjectView>
     )
 
 
--- web/src/pages/object/styles.js
+++ web/src/pages/object/styles.js
@@ -13,13 +13,48 @@ export const ObjectView = styled.div`
 ` 
 
 export const ObjectContent = styled.div`
-    width : 100%;
+    width: 100%;
     height: 100%;
-    background-color : blue;
+    background-color: blue;
+` 
+
+export const Layout = styled.div`
+    width : 100%;
+    height: auto;
+    background-color : ${ props => `${props.color}` };
+    
+    display : grid;
+    grid-template-columns : repeat( ${props => `${ props.columns }` }, 1fr );
+  
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+
+    grid-column-gap : 2rem;
+    grid-row-gap : 2rem;
 ` 
 
 export const ObjectLayout = styled.div`
     width : 100%;
     height: 100%;
-    background-color : green;
+    background-color : ${ props => `${props.color}` };
+    
+    display : grid;
+    grid-template-columns : repeat( ${props => `${ props.columns }` }, 1fr );
+  
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+
+    grid-auto-rows: 6rem;
+    grid-column-gap: 2rem;
+` 
+
+export const Input = styled.input`
+    width : 100%;
+    height: 3.5rem;
+    padding-left : 0.8rem;
+    background-color : #fff;
 ` 
